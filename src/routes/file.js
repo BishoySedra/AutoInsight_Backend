8fc48b0f4334dd767bfb4c9c500203ef3e30eed2
Mazen Mostafa <106120157+MazenMostafa2025@users.noreply.github.com>
2025-02-17 20:31:31 +0200

    adding csv python analysis route

--- src/routes/file.js
+++ src/routes/file.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import uploadfile from '../middlewares/upload/file.js';
 import { sendResponse } from '../utils/response.js';
+import { SendUrlToPython } from '../controllers/file.js';
 
 const router = Router();
 
@@ -8,5 +9,7 @@ const router = Router();
 router.post('/upload', uploadfile, (req, res) => {
     sendResponse(res, { url: req.file_url }, 'Uploaded!', 200);
 });
+
+router.post('/test-analysis', SendUrlToPython);
 
 export default router;
