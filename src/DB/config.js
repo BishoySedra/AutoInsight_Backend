4d568de2638a5bf4581e93d1c5f2c24dfd3445a0
Bishoy Sedra <bishosedra0@gmail.com>
2025-02-18 13:47:12 +0200

    Modified config.js

--- src/DB/config.js
+++ src/DB/config.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
     const mongo_host = process.env.MONGO_HOST;
     const mongo_db = process.env.MONGO_DB;
     const mongo_url = `mongodb://${mongo_username}:${mongo_password}@localhost:27017/${mongo_db}?authSource=admin`;
-    const local_url = process.env.MONGO_URI;
+    const local_url = `mongodb+srv://${mongo_username}:${mongo_password}@${mongo_host}/${mongo_db}?retryWrites=true&w=majority`;
 
     let connected_url = local_url;
 
