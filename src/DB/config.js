1d361d62f032d30d2e2c220f2f38dd8d5a0a0e73
Bishoy Sedra <bishosedra0@gmail.com>
2025-04-28 21:26:35 +0300

    Updated the DB/config.js file!

--- src/DB/config.js
+++ src/DB/config.js
@@ -20,7 +20,7 @@ const connectDB = async () => {
     const mongo_url = `mongodb://${mongo_username}:${mongo_password}@localhost:27017/${mongo_db}?authSource=admin`;
     const local_url = `mongodb+srv://${mongo_username}:${mongo_password}@${mongo_host}/${mongo_db}?retryWrites=true&w=majority`;
 
-    let connected_url = mongo_url;
+    let connected_url = local_url;
     // const url = `mongodb+srv://mazen:mazen@cluster0.devwr.mongodb.net/AutoInsights?retryWrites=true&w=majority&appName=Cluster0`;
     // await mongoose.connect(url);
     if (connected_url === local_url) {
